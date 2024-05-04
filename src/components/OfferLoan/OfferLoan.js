530bc8894cc23032b34dccb2b1ff1e789288082b
Md-Hadiuzzaman-Rifat <hadirifat100@gmail.com>
2024-05-05 01:39:43 +0600

    offer loan done

--- src/components/OfferLoan/OfferLoan.js
+++ src/components/OfferLoan/OfferLoan.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./OfferLoan.css";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
 import {useNavigate, useParams} from "react-router-dom"
@@ -8,17 +8,13 @@ import { useAuth } from "../../context/AuthContext";
 const OfferLoan = () => {
   const navigate= useNavigate()
   const {currentUser}= useAuth()
-
-  const [title, setTitle]= useState("")
-  const [cause, setCause]= useState("")
-  const [desc, setDesc]= useState("")
   const [payTime, setPayTime]= useState("")
   const [pay, setPay]= useState("")
   const [interest, setInterest]= useState("")
 
   const {id}= useParams()
 
-  const body={loan_id:id, title, cause, desc, payTime, pay, interest, currentUser}
+  const body={loan_id:id, payTime, pay, interest, currentUser}
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -36,15 +32,9 @@ const OfferLoan = () => {
     <div className="loanForm">
       <div className="loanForm__container">
       <div className="loanFrom__back" onClick={() => navigate(-1)}> <FaArrowAltCircleLeft></FaArrowAltCircleLeft> </div>
-        <h2>LOAN FORM</h2>
+        <h2>OFFER LOAN</h2>
         <form className="loanForm__form" onSubmit={handleSubmit}>
-          <div>
-            Title: Need <input type="text" placeholder="Enter your amount" onChange={e=>setTitle(e.target.value)}/>{" "}
-            for <input type="text" placeholder="write down the cause" onChange={e=>setCause(e.target.value)} />
-          </div>
-          <div>
-            Description: <input type="text" onChange={e=>setDesc(e.target.value)} />
-          </div>
+
           <div>
             Expected payback time(month):{" "}
             <input type="text" placeholder="Enter a digit"  onChange={e=>setPayTime(e.target.value)}/>
